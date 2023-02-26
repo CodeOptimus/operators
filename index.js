580ad90150a90113b8832cb83b3a5ce874cbab5e
Optimus Elikem <89908948+CodeOptimus@users.noreply.github.com>
2023-02-26 10:26:09 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+//trying to define the various courses
+const science = "Physics, Chemistry, Biology, Technical Drawing";
+const socialScience = "Accounting, Commerce, Marketing, Geography";
+const arts = "Government, Economics, Literature, History";
+const general = "English, Mathematics";
+
+
+if (student = science){
+    console.log(science + general);
+} 
+else if(student = socialScience){
+    console.log(socialScience + general);
+} 
+else if(student = arts){
+    console.log(arts + general);
+} 
+else{
+    console.log(general);
+}
+
+// What is the result when the following program is executed?
+// Result : 1815
